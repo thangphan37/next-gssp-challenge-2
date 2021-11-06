66faaad2c8d2d424c631d80de70d3b353e755276
thangphan37 <pcthangk59@gmail.com>
2021-11-06 15:05:38 +0900

    add challenge

---
+++ pages/query.tsx
@@ -0,0 +1,15 @@
+/**
+ * Requirements:
+ * 	- Render theses queries value on the screen
+ * 	- {search: 'programming'}
+ * 	- {type: 'english', location: 'us'}
+ * 	- {name: 'Myname', age: '24'}
+ * Hint:
+ * 	- Using `query` parameter.
+ */
+
+function Query() {
+  return <div>{/* add your code here */}</div>
+}
+
+export default Query
